39c94ae82a120cbc30ac174db01e2321975cea9d
Prokudina Viktoria <viktoriaprokudina76@gmail.com>
2024-11-25 16:05:07 +0500

    add section list product

--- src/App.vue
+++ src/App.vue
@@ -14,21 +14,42 @@
      </div>
      <div class="mb-3">
       <label for="date" class="form-label">Дата приема товара</label>
-     <input type="text" class="form-control" id="date" >
+     <input type="date" class="form-control" id="date" >
      </div>
      <div class="mb-3">
-      <label for="quantily" class="form-label">Количество</label>
-     <input type="text" class="form-control" id="quantily" >
+      <label for="count" class="form-label">Количество</label>
+     <input type="number" class="form-control" id="count" >
      </div>
      <div class="mb-3">
       <label for="priced" class="form-label">Цена за единицу товара</label>
-     <input type="text" class="form-control" id="prised" >
+     <input type="number" class="form-control" id="prised" >
      </div>
-     <button type="button" class="btn btn-light">Добавить</button>
+     <div class="text-center mb-3">
+      <button type="button" class="btn btn-light">Добавить</button>
+     </div>
+
    </form>
     </div>
   </div>
 </div>
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">iphone</h5>
+        <p class="card-text">25.11.24</p>
+        <p class="card-text">$200 </p>
+        <p class="card-text"> 5 шт </p>
+        <div class = "card-footer">
+          <button class="btn btn-outline-danger"> Удалить</button>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+
+
 
 </template>
 
